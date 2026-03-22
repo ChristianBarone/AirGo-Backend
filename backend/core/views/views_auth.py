b0507c9eceafd28bdef724318fd80ccda3c6275e
sergi.lopez <sergi.lopez.de.los.santos.juan@estudiantat.upc.edu>
2026-03-22 18:09:49 +0100

    Arreglo cambio foto perfil + bicing

--- backend/core/views/views_auth.py
+++ backend/core/views/views_auth.py
@@ -9,6 +9,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
 
+
 from ..serializers import GoogleAuthSerializer
 from ..models import Usuari
 
