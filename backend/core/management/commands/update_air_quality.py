ff7df523cbd9a73f008cd67f5a2421283bc77b4f
sergi.lopez <sergi.lopez.de.los.santos.juan@estudiantat.upc.edu>
2026-04-10 12:18:59 +0200

    Més punts per guardar a la bd

--- backend/core/management/commands/update_air_quality.py
+++ backend/core/management/commands/update_air_quality.py
@@ -11,9 +11,21 @@ class Command(BaseCommand):
 
         # Aquí defines los puntos de Barcelona que quieres cubrir
         puntos = [
-            {"lat": 41.385, "lon": 2.173},
-            {"lat": 41.390, "lon": 2.154},
-            # añade más puntos según necesites
+            {"lat": 41.385, "lon": 2.173},  # Cerca de Plaça Catalunya
+            {"lat": 41.390, "lon": 2.154},  # Cerca de la Sagrada Familia
+            {"lat": 41.406, "lon": 2.191},  # Barrio de Gracia
+            {"lat": 41.378, "lon": 2.192},  # Parc de la Ciutadella
+            {"lat": 41.378, "lon": 2.195},  # Barceloneta
+            {"lat": 41.414, "lon": 2.191},  # L'Eixample
+            {"lat": 41.369, "lon": 2.167},  # Zona Universitaria
+            {"lat": 41.408, "lon": 2.196},  # El Raval
+            {"lat": 41.377, "lon": 2.190},  # Poble Sec
+            {"lat": 41.423, "lon": 2.188},  # Poblenou
+            {"lat": 41.423, "lon": 2.208},  # Diagonal Mar
+            {"lat": 41.447, "lon": 2.188},  # Zona del Camp Nou
+            {"lat": 41.452, "lon": 2.175},  # Zona del Mercado de Sant Antoni
+            {"lat": 41.424, "lon": 2.191},  # Gracia
+            {"lat": 41.413, "lon": 2.164},  # Zona de Sants
         ]
 
         for punto in puntos:
