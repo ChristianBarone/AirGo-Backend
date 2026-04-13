c15e39e14e4b18811d4a7414234627e7e7bba459
ona.regue <ona.regue@estudiantat.upc.edu>
2026-04-14 00:38:48 +0200

    afegir import

--- backend/core/views/views_air_quality.py
+++ backend/core/views/views_air_quality.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny
 from ..services.air_quality import get_air_quality_near
+import requests
 
 
 class AirQualityView(APIView):
