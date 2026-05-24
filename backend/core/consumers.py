90dc68ed7607fc8dfd6b9ae2df4b09709e6bdbb7
sergi.lopez <sergi.lopez.de.los.santos.juan@estudiantat.upc.edu>
2026-05-24 19:39:55 +0200

    linterns

--- backend/core/consumers.py
+++ backend/core/consumers.py
@@ -139,7 +139,9 @@ class ChatConsumer(AsyncWebsocketConsumer):
                     "conversa_id": str(self.conversa.pk),
                     "emissor_id": str(self.usuari.pk),
                     "emissor_username": self.usuari.username,
-                    "emissor_profile_pic": self.usuari.profile_pic.url if self.usuari.profile_pic else "",
+                    "emissor_profile_pic": (
+                        self.usuari.profile_pic.url if self.usuari.profile_pic else ""
+                    ),
                     "contingut": contingut[:100],
                     "timestamp": str(int(missatge.enviat_at.timestamp())),
                 },
